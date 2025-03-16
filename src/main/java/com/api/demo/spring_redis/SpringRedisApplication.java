ffdcee9f27924cb8e02cd8b50ec2569db1064041
Phatdoan203 <phatdoan.1014@gmail.com>
2025-03-17 00:20:35 +0700

    Initital Commit

---
+++ src/main/java/com/api/demo/spring_redis/SpringRedisApplication.java
@@ -0,0 +1,13 @@
+package com.api.demo.spring_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/demo/spring_redis/SpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.demo.spring_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
